bc25292fb65faec7a3bf63fe3ab49745e159531c
hali <km@mezquita.jp>
2026-08-27 15:39:28 +0900

    wip

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
 // このファイルは npm run build のたびに gen-version.js が自動生成します。手動編集しないでください。
-export const BUILD_VERSION = "2026-08-27T06:35:50.671Z";
+export const BUILD_VERSION = "2026-08-27T06:39:25.944Z";
